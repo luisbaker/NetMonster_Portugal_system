0c1c0dd15a2805d50142e2c084ad3e164efa1376
luisbaker <iluisferreira1516@gmail.com>
2023-12-12 04:12:08 +0000

    Mudanças no codigo base adicionei novas coisas etc

--- public/js/multipleEnbCharts.js
+++ public/js/multipleEnbCharts.js
@@ -6,15 +6,11 @@ async function loadCSV(filePath) {
       delimiter: ";",
       step: function (row) {
         try {
-          // Ignore rows that don't start with "4G"
           if (!row.data[0].startsWith("4G")) {
             return;
           }
-
           console.log(`Processing row: ${row.data}`);
-
-          let enb = row.data[5]; // Assuming eNB is always at index 5 for 4G
-
+          let enb = row.data[5]; 
           if (enb) {
             console.log(`Found eNB: ${enb}`);
             enbs.add(enb);
@@ -38,14 +34,13 @@ async function loadCSV(filePath) {
   });
 }
 
-// Função para criar um gráfico circular
 function createPieChart(canvasId, label, data, total) {
   const percentual = (data / total) * 100;
   const ctx = document.getElementById(canvasId).getContext('2d');
   new Chart(ctx, {
     type: 'doughnut',
     data: {
-      labels: [`${label} (${data})`, `Outros (${total - data})`],
+      labels: [`Antenas identificadas (${data})`, `Por identificar (${total - data})`],
       datasets: [{
         data: [percentual, 100 - percentual],
         backgroundColor: ['#36a2eb', '#d3d3d3'],
@@ -69,23 +64,29 @@ function createPieChart(canvasId, label, data, total) {
 
 async function generateCharts() {
   const percentuais = {
-    'NOS': 4874,
-    'Vodafone': 5074,
-    'Digi': 2000,
-    'MEO': 5048
+    '26201': 4874, // NOS
+    '26202': 5074, // Vodafone
+    '26203': 2000, // Digi
+    '26204': 5048  // MEO
   };
 
-  const operadoras = ['NOS', 'Vodafone', 'Digi', 'MEO'];
+  const operadoras = ['26201', '26202', '26203', '26204'];
+  const operadoraNames = {
+    '26201': 'NOS',
+    '26202': 'Vodafone',
+    '26203': 'Digi',
+    '26204': 'MEO'
+  };
 
   for (const operadora of operadoras) {
     try {
       const operadoraENBCount = await loadCSV(`/database/${operadora}_PT.ntm`);
       console.log(`${operadora} data:`, operadoraENBCount);
 
-      createPieChart(`${operadora.toLowerCase()}Canvas`, operadora, operadoraENBCount, percentuais[operadora]);
-      console.log(`Created chart for ${operadora}`);
+      createPieChart(`${operadora.toLowerCase()}Canvas`, operadoraNames[operadora], operadoraENBCount, percentuais[operadora]);
+      console.log(`Created chart for ${operadoraNames[operadora]}`);
     } catch (error) {
-      console.error(`Error generating chart for ${operadora}:`, error);
+      console.error(`Error generating chart for ${operadoraNames[operadora]}:`, error);
     }
   }
 }
