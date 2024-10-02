add61f31589e05226cfed7c838c4cca711ba73e5
Alan Hoffmeister <alanhoffmeister@gmail.com>
2024-10-02 14:21:36 -0300

    chore: fix gNB

--- server/libs/ntm-to-cell.test.js
+++ server/libs/ntm-to-cell.test.js
@@ -3,6 +3,7 @@ import { describe, it } from "node:test";
 import { ntmToCell } from "./ntm-to-cell.js";
 
 const TEST_NTM = "4G;268;06;1792553;48290;0;21;39.219198573285;-9.295696914196;Capelas, Lourinhã;500";
+const TEST_5G = "5G;268;03;544964740;265;2128768;156;41.2142;-8.1711;R. Bairro Pinheiro Manso 295, Constance;156750";
 
 describe("ntmToCell", () => {
 	it("should convert a NTM entry to a cell object", () => {
@@ -19,6 +20,23 @@ describe("ntmToCell", () => {
 			technology: "4G",
 			enb: 0,
 			location: "Capelas, Lourinhã",
+		});
+	});
+
+	it("should convert 5G ntm entry to a cell object", () => {
+		const result = ntmToCell(TEST_5G);
+
+		assert.deepStrictEqual(result, {
+			area: 265,
+			cid: 544964740,
+			code: 156,
+			frequency: 156750,
+			latitude: 41.2142,
+			longitude: -8.1711,
+			network: { mcc: 268, mnc: 3 },
+			technology: "5G",
+			gnb: 2128768,
+			location: "R. Bairro Pinheiro Manso 295, Constance",
 		});
 	});
 });
